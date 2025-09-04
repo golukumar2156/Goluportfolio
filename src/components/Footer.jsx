cc8decb1b0d9998dace92a099a125bea48dfce17
Golu <golusiddharth88@gmail.com>
2025-09-04 11:47:35 +0530

    changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,61 +1,48 @@
-import React from "react";
+ import React from "react";
 import { FaLocationArrow } from "react-icons/fa";
-
 function Footer() {
   return (
-    <div className="">
-<div className="w-full max-w-[1450px] mx-auto  mt-4 px-4 min-h-[230px] bg-gradient-to-r from-[#33565c] to-[#279e94] rounded-xl shadow-lg transition delay-40 duration-400 ease-in-out hover:-translate-y-1.5 hover:scale-102 hover:border-green-400 shadow-blue-400/80">
+    <div>
+      <div className="xl:w-[1450px] xl:h-[300px] xl:ml-[44px] xl:mt-[10px]  lg:h-[300px] lg:w-[955px]  lg:ml-[25px] md:h-[220px] md:w-[715px]  md:ml-[25px]  h-[260px] w-[350px]  ml-[20px] mt-[10px] bg-gradient-to-r from-[#33565c] to-[#279e94] rounded-xl shadow-lg   transition delay-40 duration-400 ease-in-out hover:-translate-y-1.5 hover:scale-102 hover:border-green-400  shadow-blue-400/80  ">
         <div>
-          <h1 className="text-center pt-6 text-2xl md:text-3xl lg:text-4xl font-extrabold text-gray-300">
+          <h1 className="xl:pl-[520px] xl:pt-[40px] xl:text-[40px] lg:pl-[300px] lg:pt-[30px] lg:text-[35px] md:pl-[170px] md:pt-[30px] md:text-[30px] pl-[20px] pt-[20px] text-[25px] font-[800]  text-gray-300">
             Ready to Start a Project?
           </h1>
         </div>
-
-        <div className="pt-3 text-sm md:text-base lg:text-lg text-[#c0e6ec] font-[400] text-center px-4">
-          {/* Desktop */}
-          <p className="hidden sm:block">
+        <div className=" xl:pt-[6px]  xl:text-[20px] lg:text-[20px] md:text-[15px] text-[15px] text-[#c0e6ec] font-[400] ">
+          <p className="hidden sm:block xl:pl-[400px] lg:pl-[160px] lg:pt-[10px] md:pl-[90px] md:pt-[5px]">
             I'm always excited to work on new database projects and learn from
             experienced
           </p>
-          {/* Mobile */}
-          <p className="sm:hidden">
-            I'm always excited to work on new database <br />
-            projects and learn from experienced
+          {/*first for mobile  */}
+          <p className="sm:hidden pl-[25px] pt-[10px] ">
+            I'm always excited to work on new database <br /> <p className="pl-[25px]">projects and learn from experienced</p>
           </p>
-
-          {/* Desktop */}
-          <p className="hidden sm:block">
+          {/* end of mobile */}
+          <p className=" hidden sm:block xl:pl-[490px] lg:pl-[250px] lg:pt-[5px] md:pl-[155px] md:pt-[5px]">
             developers. Let's discuss how I can contribute to your team!
           </p>
-          {/* Mobile */}
-          <p className="sm:hidden">
-            developers. Let's discuss how I can contribute <br />
-            to your team!
+          {/* second for mobile  */}
+           <p className="sm:hidden pl-[20px] pt-[5px]">
+            developers. Let's discuss how I can contribute <br /> <p className="pl-[90px]">to your team!</p>
           </p>
-
-          {/* Buttons */}
-          <div className="flex justify-center gap-4 sm:gap-10 mt-6">
-            <button className="bg-gray-300 hover:text-black font-[600] duration-1000 cursor-pointer hover:bg-gradient-to-r from-[#bcc5c6] to-[#79e0d7] text-[#2a474c] py-2 px-4 md:px-6 md:py-2 rounded hover:bg-gray-800">
-              <div className="flex items-center gap-3">
-                Get In Touch
-                <FaLocationArrow />
-              </div>
+          {/* end of mobile */}
+          <div className="flex sm:gap-10 gap-5 justify-center xl:mt-[15px]    lg:mt-[30px]   md:mt-[20px]  mt-[25px]">
+            <button className="bg-gray-300 hover:text-black  duration-1000 cursor-pointer hover:bg-gradient-to-r from-[#bcc5c6] to-[#79e0d7] text-[#2a474c]   xl:py-2  lg:py-2  lg:px-5 md:px-4 md:py-2 px-2 py-1    rounded hover:bg-gray-800">
+             <div className="flex items-center gap-3">
+                 Get In Touch
+                 <FaLocationArrow />
+             </div>
             </button>
-            <button className="bg-gray-300 hover:text-black font-[600] duration-1000 cursor-pointer hover:bg-gradient-to-r from-[#bcc5c6] to-[#79e0d7] text-[#2a474c] py-2 px-4 md:px-6 md:py-2 rounded hover:bg-gray-800">
-              View My Project
+            <button className="bg-gray-300 hover:text-black  duration-1000 cursor-pointer hover:bg-gradient-to-r from-[#bcc5c6] to-[#79e0d7] text-[#2a474c]   xl:py-2  xl:px-6 lg:py-2  lg:px-6 md:px-4 md:py-2 px-3 py-1  rounded hover:bg-gray-800">
+              View My Project 
             </button>
           </div>
         </div>
       </div>
-
-      {/* Footer bottom bar */}
-      <div className="flex flex-col sm:flex-row justify-between items-center h-auto sm:h-[50px] w-full bg-gray-700 mt-10 rounded-t-lg px-4 py-2">
-        <p className="text-[#c0e6ec] font-[500] text-center sm:text-left">
-          All rights reserved
-        </p>
-        <p className="text-[#c0e6ec] font-[500] text-center sm:text-right">
-          © 2023 Golu Siddharth
-        </p>
+      <div className="flex justify-between  xl:h-[50px]  w-full bg-gray-700 mt-[60px] xl:rounded-t-[10px]">
+      <p className="text-[#c0e6ec] font-[500] xl:pt-[5px] xl:pl-[5px]">All rights reserved</p>
+      <p className="text-[#c0e6ec] font-[500] xl:pt-[5px] xl:pr-[5px]">© 2023 Golu Siddharth</p>
       </div>
     </div>
   );
