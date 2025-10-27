11e67307067fd6dad19371403e92e191002afcc6
Golu <golusiddharth88@gmail.com>
2025-10-27 22:32:07 +0530

    some change in skills stacks

--- src/components/Skillscard.jsx
+++ src/components/Skillscard.jsx
@@ -1,111 +1,74 @@
 import React from "react";
 
 function SkillsCard() {
+  const skills = [
+    {
+      title: "Frontend Development",
+      tools: [
+        "HTML5",
+        "Tailwind CSS / Bootstrap",
+        "JavaScript",
+        "React.js",
+        "Redux/Context API",
+        "Axios",        
+      ],
+      level: "Intermediate",
+    },
+    {
+      title: "Backend Development",
+      tools: [
+        "Java",
+        "SpringBoot",
+        "Spring Security",
+        "Spring Data JPA",
+        "Lombok",
+        "MySQL",
+      ],
+      level: "Intermediate",
+    },
+    {
+      title: "Core CS Concepts",
+      tools: [
+        "C and C++",
+        "Operating System",
+        "Database Management System",
+        "Data Structures & Algorithms",
+        "Object Oriented Programming",
+        "Software Engineering",
+      ],
+      level: "Intermediate",
+    },
+  ];
   return (
     <div className=" md:py-[40px] md:px-4 px-2 pt-[50px] ">
       <h1 className="text-4xl xl:pb-[20px] xl:pt-[60px] md:pt-[100px] lg:pt-[90px] font-bold text-black text-center ">
         Technical Skills
       </h1>
-      <div className="md:flex md:gap-[10px] lg:gap-[30px] xl:gap-[15px] xl:px-[10px] py-3 px-3 ">
-        <div className="mt-[30px]  bg-gradient-to-r from-[#46818b] to-[#0F766E]  rounded-xl shadow-lg p-6 xl:w-[400px]  
-transition-transform duration-400 ease-in-out 
-  hover:-translate-y-1.5 hover:scale-105 focus:scale-105 active:scale-105          hover:border-green-400  shadow-blue-400/50  xl:ml-[27px] ">
-          <h4 className="text-2xl font-semibold text-black mb-4">
-            Forntend Development
-          </h4>
-          <ul className="flex flex-col gap-4">
-            <li className="from-blue-900 rounded-lg p-4 shadow-md flex flex-col">
-              <strong className="text-black">HTML5</strong>
-              <small className="text-black">Intermediate</small>
-            </li>
-            <li className="from-blue-900 rounded-lg p-4 shadow-md flex flex-col">
-              <strong className="text-black">CSS</strong>
-              <small className="text-black">Intermediate</small>
-            </li>
-            <li className="from-blue-900 rounded-lg p-4 shadow-md flex flex-col">
-              <strong className="text-black">JavaScript</strong>
-              <small className="text-black">Intermediate</small>
-            </li>
-            <li className="from-blue-900 rounded-lg p-4 shadow-md flex flex-col">
-              <strong className="text-black">ReactJS</strong>
-              <small className="text-black">Intermediate</small>
-            </li>
-            <li className="from-blue-900 rounded-lg p-4 shadow-md flex flex-col">
-              <strong className="text-black">Tailwind / Bootstrap</strong>
-              <small className="text-black">Intermediate</small>
-            </li>
-          </ul>
-        </div>
 
-       <div className="mt-[30px] bg-gradient-to-r from-[#46818b] to-[#0F766E] text-white 
-  rounded-xl shadow-lg p-6 xl:w-[400px] xl:ml-[100px] 
-  transition-transform duration-400 ease-in-out 
-  hover:-translate-y-1.5 hover:scale-105 focus:scale-105 active:scale-105 
-  hover:border-green-400 shadow-blue-400/50"
->
-  <h4 className="text-2xl font-semibold text-black mb-4">
-    Backend Development
-  </h4>
-  <ul className="flex flex-col gap-4">
-    <li className="rounded-lg p-4 shadow-md flex flex-col">
-      <strong className="text-black">Java</strong>
-      <small className="text-black">Intermediate</small>
-    </li>
-    <li className="rounded-lg p-4 shadow-md flex flex-col">
-      <strong className="text-black">Spring Boot</strong>
-      <small className="text-black">Intermediate</small>
-    </li>
-    <li className="rounded-lg p-4 shadow-md flex flex-col">
-      <strong className="text-black">REST APIs</strong>
-      <small className="text-black">Intermediate</small>
-    </li>
-    <li className="rounded-lg p-4 shadow-md flex flex-col">
-      <strong className="text-black">MySQL / SQL</strong>
-      <small className="text-black">Intermediate</small>
-    </li>
-    <li className="rounded-lg p-4 shadow-md flex flex-col">
-      <strong className="text-black">Spring Data JPA</strong>
-      <small className="text-black">Intermediate</small>
-    </li>
-  </ul>
-</div>
-
-
-        <div className="mt-[30px] bg-gradient-to-r from-[#46818b] to-[#0F766E] text-white rounded-xl shadow-lg p-6 xl:w-[400px] xl:ml-[100px] mb-6 sm:mb-0
-transition-transform duration-400 ease-in-out 
-  hover:-translate-y-1.5 hover:scale-105 focus:scale-105 active:scale-105 hover:border-green-400 shadow-blue-400/50">
-          <h4 className="text-2xl font-semibold text-black mb-4">
-            Core CS Concepts
-          </h4>
-          <ul className="flex flex-col gap-4">
-            <li className="rounded-lg p-4 shadow-md flex flex-col">
-              <strong className="text-black">C and C++ </strong>
-              <small className="text-black">Intermediate</small>
-            </li>
-            <li className="rounded-lg p-4 shadow-md flex flex-col">
-              <strong className="text-black">Operating System (OS)</strong>
-              <small className="text-black">Intermediate</small>
-            </li>
-            <li className="rounded-lg p-4 shadow-md flex flex-col">
-              <strong className="text-black">
-                Database Management System (DBMS)
-              </strong>
-              <small className="text-black">Intermediate</small>
-            </li>
-            <li className="rounded-lg p-4 shadow-md flex flex-col">
-              <strong className="text-black">
-                Data Structures & Algorithms (DSA)
-              </strong>
-              <small className="text-black">intermediate</small>
-            </li>
-            <li className="rounded-lg p-4 shadow-md flex flex-col">
-              <strong className="text-black">
-                Object Oriented Programming (OOP)
-              </strong>
-              <small className="text-black">intermediate</small>
-            </li>
-          </ul>
-        </div>
+      <div className="md:flex md:gap-[10px] mb-4 lg:gap-[30px] xl:gap-[80px] xl:px-[10px] py-3 px-3 ">
+        {skills.map((skill, index) => (
+          <div
+            key={index}
+            className="mt-[30px]  bg-gradient-to-r from-[#46818b] to-[#0F766E]  rounded-xl shadow-lg p-6 xl:w-[400px]  
+             transition-transform duration-400 ease-in-out 
+             hover:-translate-y-1.5 hover:scale-105 focus:scale-105 active:scale-105   hover:border-green-400  shadow-blue-400/50  xl:ml-[27px] "
+          >
+            <h4 className="text-2xl font-semibold text-black mb-4">
+              {skill.title}
+            </h4>
+            <ul className="flex flex-col gap-4">
+              {skill.tools.map((tool, idx) => (
+                <li
+                  key={idx}
+                  className="from-blue-900 rounded-lg p-4 shadow-md flex flex-col"
+                >
+                  <strong className="text-black">{tool}</strong>
+                  <small className="text-black">{skill.level}</small>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
       </div>
     </div>
   );
